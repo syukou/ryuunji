2d5419a161c5a216cd3cd1b1add1ddb77f364c07
田口 宗孝 <m.taguchi@leihauoli.com>
2016-04-29 16:44:21 +0900

    add files -> add

--- js/jquery.bxslider.top.ini.js
+++ js/jquery.bxslider.top.ini.js
@@ -1,5 +1,9 @@
 // JavaScript Document
 
+$(window).on('load',function(){
+    $('#jsi-slidearea').css({visibility:'hide',opacity:'0'}).animate({visibility:'visible',opacity:'1'},1000);
+});
+
 $(document).ready(function(){
 var obj = $('#jsi-slidearea .bxslider').bxSlider({
 	  auto: true,
