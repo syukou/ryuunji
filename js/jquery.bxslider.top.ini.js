9ae1897635360f3bb73fb9cc9fef5786db6d40e3
田口 宗孝 <m.taguchi@leihauoli.com>
2016-05-12 16:40:54 +0900

    add files -> add

--- js/jquery.bxslider.top.ini.js
+++ js/jquery.bxslider.top.ini.js
@@ -18,33 +18,3 @@ var obj = $('#jsi-slidearea .bxslider').bxSlider({
 	  autoHover: true
 	});
 });
-
-$(document).ready(function(){
-var homeInfo = $('#topInfo-slider .slider').bxSlider({
-	  auto: true,
-	  autoControls: false,
-	  infiniteLoop: true,
-	  pager: false,
-	  mode: 'vertical', /* fade,vertical など */
-	  speed: 750,
-	  pause: 3000,
-	  onSlideAfter: function () { homeInfo.startAuto(); },//一旦停止後また再生
-	  easing: 'jswing',
-	  autoHover: true,
-	  nextSelector: '#topInfo-next',
-	  prevSelector: '#topInfo-prev',
-	  nextImage: '../img/nav-next.png',
-	  prevImage: '../img/nav-prev.png'
-	});
-});
-
-$(document).ready(function(){
-var officeImg = $('#ticker .bxslider').bxSlider({
-	  ticker: true,
-		minSlides: 3,
-		maxSlides: 10,
-		slideWidth: 216,
-		slideMargin: 0,
-		speed: 200000
-	});
-});
